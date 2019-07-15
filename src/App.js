72c2f68e01c723e949d570c567e2e31db1d15473
wangying116564 <wangying116564@pwrd.com>
2019-07-15 17:10:31 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">add</Button>
+    </div>
+  );
+}
+
+export default App;
