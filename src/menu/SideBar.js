fe912d56ae6dcedd07f3208e48006e1d99da9ed1
wangying116564 <wangying116564@pwrd.com>
2019-07-15 18:08:14 +0800

    sidebar

--- src/menu/SideBar.js
+++ src/menu/SideBar.js
@@ -1,60 +1,69 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Icon } from 'antd';
+import '../static/css/menu/SideBar.css';
 
-const { Header, Sider, Content } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 
 class SideBar extends Component {
     state = {
         collapsed: false,
     };
-    
+
     toggle = () => {
         this.setState({
             collapsed: !this.state.collapsed,
         });
     };
-    
     render() {
         return (
-            <Layout>
-            <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
-                    <Icon type="user" />
-                    <span>nav 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                    <Icon type="video-camera" />
-                    <span>nav 2</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                    <Icon type="upload" />
-                    <span>nav 3</span>
-                </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout>
-                <Header style={{ background: '#fff', padding: 0 }}>
-                <Icon
-                    className="trigger"
-                    type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                    onClick={this.toggle}
-                />
-                </Header>
-                <Content
-                style={{
-                    margin: '24px 16px',
-                    padding: 24,
-                    background: '#fff',
-                    minHeight: 280,
-                }}
-                >
-                Content
-                </Content>
+            <Layout className="container">
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span className="nav-text">图表</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span className="nav-text">列表</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span className="nav-text">文本</span>
+                        </Menu.Item>
+                        <SubMenu
+                            key="4"
+                            title={
+                                <span>
+                                <Icon type="user" />
+                                <span>权限管理</span>
+                                </span>
+                            }
+                            >
+                            <Menu.Item key="4.1">Tom</Menu.Item>
+                            <Menu.Item key="4.2">Bill</Menu.Item>
+                            <Menu.Item key="4.3">Alex</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', paddingLeft: '20px' }}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Content style={{ margin: '24px 16px 0' }}>
+                        <div style={{ padding: 24, background: '#fff' }}>content</div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                </Layout>
             </Layout>
-            </Layout>
+            // mountNode,
         );
     }
 }
